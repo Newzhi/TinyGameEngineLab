569251b48e85604936637282c55cf7c2ec29a662
Newzhi <2330673541@qq.com>
2026-06-06 01:11:12 +0800

    111

--- LearnOpenGL/Proj/src/main.cpp
+++ LearnOpenGL/Proj/src/main.cpp
@@ -85,7 +85,7 @@ int main()
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "   FragColor = vec4(0.5f, 0.5f, 0.2f, 1.0f);\n"
         "}\0";
 
     float vertices[] = {
